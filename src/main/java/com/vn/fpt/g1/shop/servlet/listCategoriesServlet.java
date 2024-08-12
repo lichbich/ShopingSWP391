66c45470b6cda4898a2799dc68212e8dd880ce5c
locpx <locpx5120@gmail.com>
2024-08-12 11:12:40 +0700

    update CRUD Category

--- src/main/java/com/vn/fpt/g1/shop/servlet/listCategoriesServlet.java
+++ src/main/java/com/vn/fpt/g1/shop/servlet/listCategoriesServlet.java
@@ -2,23 +2,25 @@ package com.vn.fpt.g1.shop.servlet;
 
 import com.vn.fpt.g1.shop.dao.CategoryDAO;
 import com.vn.fpt.g1.shop.entity.Category;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/listCategories")
-public class ListCategoryServlet extends HttpServlet {
-
+@WebServlet(name = "listCategoriesServlet", value = "/listCategoriesServlet")
+public class listCategoriesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         CategoryDAO categoryDAO = new CategoryDAO();
         List<Category> categoryList = categoryDAO.getAllCategories();
         request.setAttribute("categories", categoryList);
         request.getRequestDispatcher("/WEB-INF/saleManager/listCategories.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
     }
 }
